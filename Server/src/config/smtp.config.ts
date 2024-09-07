bf2ef606bdfa813f62a2a65516c5054b4ecd17ad
Samarth Patel <patelsamarth250@gmail.com>
2024-09-07 23:33:11 +0530

    change email config

--- Server/src/config/smtp.config.ts
+++ Server/src/config/smtp.config.ts
@@ -4,8 +4,8 @@ const transporter = createTransport({
   port: 465,
   host: "smtp.gmail.com",
   auth: {
-    user: "satyampundir03@gmail.com",
-    pass: "vrbc nvam ckvb vsds",
+    user: "patelsamarth000@gmail.com",
+    pass: "ctif kcsq ahsu bpve",
   },
   secure: true,
 });
